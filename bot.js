9912b0066b8bf69d8216109426773cc708397111
orcutt989 <james.orcutt@puppet.com>
2019-09-11 12:12:01 -0500

    logger fixes

--- bot.js
+++ bot.js
@@ -1,21 +1,22 @@
+
 var Discord = require('discord.io');
-var logger = require ('winston');
+var logger = require('winston');
 var auth = require('./auth.json');
-//Configure logger settings
+// Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-//Initialize Discord Bot
+// Initialize Discord Bot
 var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
+   token: auth.token,
+   autorun: true
 });
-bot.on('ready', function (evt){
+bot.on('ready', function (evt) {
     logger.info('Connected');
-    logger.info('Logged in as : ');
-    logger.info(bot.username + '- (' + bot.id + ')');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
